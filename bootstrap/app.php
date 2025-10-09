24b613a51079e40dccf99c7b5f4d7407c8ce5c86
Tanzeel <tanzeeltariq39@gmail.com>
2025-10-09 05:15:15 +0500

    wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,11 +11,13 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->validateCsrfTokens(except: [
-            '/login',
-            '/register',
-        ]);
+        $middleware->statefulApi();
+        // $middleware->validateCsrfTokens(except: [
+        //     '/login',
+        //     '/register',
+        // ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,34 @@
 
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login',[AuthController::class,'login']);
-Route::post('/register',[AuthController::class,'register']);
+// Route::post('/login',[AuthController::class,'login']);
+// Route::post('/register',[AuthController::class,'register']);
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only('email', 'password');
+
+    if (Auth::attempt($credentials)) {
+        // User authenticated successfully
+        return response()->json([
+            'message' => 'Login successful!',
+        ]);
+    } else {
+        return response()->json([
+            'message' => 'Invalid login credentials!',
+        ], 401);
+    }
+});
+
+Route::post('/logout', function () {
+    auth()->logout();
+
+    return response()->json([
+        'message' => 'Successfully logged out!',
+    ]);
+});
 
 
 Route::get('/user', function (Request $request) {
