ea759ac0c49aa7da6c77fbfcafe5a6cfcbc62746
Tanzeel <tanzeeltariq39@gmail.com>
2025-11-12 22:30:11 +0500

    fixed

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => ['http://sobi.test'],
+    'allowed_origins_patterns' => ['https://sobirentals.com'],
 
     'allowed_headers' => ['*'],
 
